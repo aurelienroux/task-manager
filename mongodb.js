4917845fc45bcd23af1e83d1b3ec04495b818661
Aurélien Roux <aureroux13@gmail.com>
2020-11-26 14:57:10 -0500

    mongo create operation

--- mongodb.js
+++ mongodb.js
@@ -9,8 +9,33 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
   const db = client.db(databaseName)
   
-  db.collection('users').insertOne({
-    name: "Aure",
-    age: 39
-  })
+  // db.collection('users').insertOne({
+  //   name: "Aure",
+  //   age: 39
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.log('unable to insert user')
+  //   }
+      
+  //   console.log(result.ops)
+  // })
+
+  // db.collection('users').insertMany([
+  //   { name: 'David', age: 40 },
+  //   { name: 'Adrien', age: 7 }
+  // ], (error, result) => {
+  //     if (error) return console.log('unable to insert multiple users')
+      
+  //     console.log(result.ops)
+  // })
+
+  // db.collection('tasks').insertMany([
+  //   { description: 'buy bass', completed: true },
+  //   { description: 'tune bass', completed: true },
+  //   { description: 'learn songs', completed: false },
+  // ], (error, response) => {
+  //     if (error) return console.log('Unable to insert tasks')
+
+  //     console.log(response.ops)
+  // })
 })
