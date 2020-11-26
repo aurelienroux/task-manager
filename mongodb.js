9ef9e22a36ee2246ded5f897cfa8559c3438a3ba
Aurélien Roux <aureroux13@gmail.com>
2020-11-26 15:31:43 -0500

    object id constructor

--- mongodb.js
+++ mongodb.js
@@ -1,41 +1,49 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-  if (error) return console.log('Unable to connect to db', error)
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
 
-  const db = client.db(databaseName)
-  
-  // db.collection('users').insertOne({
-  //   name: "Aure",
-  //   age: 39
-  // }, (error, result) => {
-  //   if (error) {
-  //     return console.log('unable to insert user')
-  //   }
-      
-  //   console.log(result.ops)
-  // })
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error, client) => {
+    if (error) return console.log("Unable to connect to db", error);
 
-  // db.collection('users').insertMany([
-  //   { name: 'David', age: 40 },
-  //   { name: 'Adrien', age: 7 }
-  // ], (error, result) => {
-  //     if (error) return console.log('unable to insert multiple users')
-      
-  //     console.log(result.ops)
-  // })
+    const db = client.db(databaseName);
 
-  // db.collection('tasks').insertMany([
-  //   { description: 'buy bass', completed: true },
-  //   { description: 'tune bass', completed: true },
-  //   { description: 'learn songs', completed: false },
-  // ], (error, response) => {
-  //     if (error) return console.log('Unable to insert tasks')
+    db.collection('users').insertOne({
+      _id: id,
+      name: "Nathan",
+      age: 6
+    }, (error, result) => {
+      if (error) {
+        return console.log('unable to insert user')
+      }
 
-  //     console.log(response.ops)
-  // })
-})
+      console.log(result.ops)
+    })
+
+    // db.collection('users').insertMany([
+    //   { name: 'David', age: 40 },
+    //   { name: 'Adrien', age: 7 }
+    // ], (error, result) => {
+    //     if (error) return console.log('unable to insert multiple users')
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('tasks').insertMany([
+    //   { description: 'buy bass', completed: true },
+    //   { description: 'tune bass', completed: true },
+    //   { description: 'learn songs', completed: false },
+    // ], (error, response) => {
+    //     if (error) return console.log('Unable to insert tasks')
+
+    //     console.log(response.ops)
+    // })
+  }
+);
