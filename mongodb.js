f5f3752bfe1bd43d6eb62e07ec16cd5e53725f59
Aurélien Roux <aureroux13@gmail.com>
2020-11-26 17:33:21 -0500

    80 updating documents

--- mongodb.js
+++ mongodb.js
@@ -11,31 +11,22 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection("users").findOne(
-    //   { _id: new ObjectId("5fc00dedc31cfe3516de0016") },
-    //   (err, user) => {
-    //     if (err) return console.log("Unable to find user");
-
-    //     console.log(user);
+    // db.collection("users").updateOne(
+    //   { _id: new ObjectId("5fc0005e795ea732f29c3569") },
+    //   {
+    //     $set: { name: 'Etienne' }
     //   }
-    // );
+    // )
+    // .then(data => console.log('end', data))
+    // .catch(err => console.log(err))
 
-    // db.collection('users').find({ age: 39 }).toArray((err, users) => {
-    //   console.log(users)
-    // })
-
-    // db.collection('users').find({ age: 39 }).count((err, count) => {
-    //   console.log(count)
-    // })
-
-    db.collection('tasks').findOne({
-      _id: new ObjectId("5fc0066aedaef333f9a542c8"),
-    }, (err, task) => {
-        console.log(task)
-    });
-
-    db.collection('tasks').find({ completed: true }).toArray((err, tasks) => {
-      console.log(tasks)
-    })
+    db.collection('tasks').updateMany(
+      { completed: false },
+      {
+        $set: { completed: true }
+      }
+    )
+      .then(data => console.log(data.modifiedCount))
+      .catch(err => console.log(err))
   }
 );
