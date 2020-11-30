c7c61af0d616bf817687c54fda4b1d13711d60f8
Aurélien Roux <aureroux13@gmail.com>
2020-11-30 13:47:16 -0500

    task creation endpoint

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,21 +1,7 @@
 const mongoose = require('mongoose')
 
-const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api'
-
-mongoose.connect(connectionURL, {
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
-})
-
-const Task = mongoose.model('task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
 })
