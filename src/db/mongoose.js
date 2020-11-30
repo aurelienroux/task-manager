1b07fdc08a29958be5be15eb47e65e6c91af5ed9
Aurélien Roux <aureroux13@gmail.com>
2020-11-30 11:01:27 -0500

    89 user endpoint

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,62 +1,12 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
-const connectionURL = "mongodb://127.0.0.1:27017/task-manager-api";
+const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api'
 
 mongoose.connect(connectionURL, {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useUnifiedTopology: true,
-});
-
-const User = mongoose.model('user', {
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) { 
-      if (!validator.isEmail(value)) {
-        throw new Error('Email format is invalid')
-      }
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error('Invalid password')
-      }
-    }
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be positive')
-      }
-    }
-  }
+  useUnifiedTopology: true
 })
-
-// const nathan = new User({
-//   name: '   Nathan   ',
-//   email: '   ADrien@test.ca',
-//   password: 'the password is ok'
-// })
-
-// nathan.save()
-//   .then(res => console.log(res))
-//   .catch(err => console.log('err', err))
 
 const Task = mongoose.model('task', {
   description: {
@@ -69,12 +19,3 @@ const Task = mongoose.model('task', {
     default: false
   }
 })
-
-const task = new Task({
-  description: 'GB bass',
-  // completed: true
-})
-
-task.save()
-  .then(res => console.log(res))
-  .catch(err => console.log(err))
---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('user', {
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email format is invalid')
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Invalid password')
+      }
+    }
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be positive')
+      }
+    }
+  }
+})
+
+module.exports = User
