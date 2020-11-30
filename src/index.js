160872925d2dca683ece7c1a9d0056da14b98887
Aurélien Roux <aureroux13@gmail.com>
2020-11-30 14:54:04 -0500

    tasks reading endpoints

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+// USERS ROUTES
 app.post('/users', (req, res) => {
   const user = new User(req.body)
   user
@@ -20,7 +21,7 @@ app.post('/users', (req, res) => {
 app.get('/users', (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch(() => res.status(500).send())
+    .catch((err) => res.status(500).send(err))
 })
 
 app.get('/users/:id', (req, res) => {
@@ -34,12 +35,30 @@ app.get('/users/:id', (req, res) => {
     .catch((err) => res.status(500).send(err))
 })
 
+// TASKS ROUTES
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body)
   task
     .save()
     .then(() => res.status(201).send(task))
     .catch((err) => res.status(400).send(err))
+})
+
+app.get('/tasks', (req, res) => {
+  Task.find({})
+    .then((tasks) => res.send(tasks))
+    .catch((err) => res.status(500).send(err))
+})
+
+app.get('/tasks/:id', (req, res) => {
+  const { id } = req.params
+  Task.findById({ _id: id })
+    .then((task) => {
+      if (!task) return res.status(404).send('No task found')
+
+      return res.send(task)
+    })
+    .catch((err) => res.status(500).send(err))
 })
 
 app.listen(port, () => console.log(`Server is live on port ${port}`))  // eslint-disable-line
