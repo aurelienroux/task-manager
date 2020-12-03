b25e45f90e13b345f20a3ca0d266ece937c7d123
Aurélien Roux <aureroux13@gmail.com>
2020-12-03 15:45:13 -0500

    96 async await

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,14 +1,24 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5fc5147e24b468322a72d9c9', { age: 1 })
-  .then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 1 })
-  })
-  .then((result) => {
-    console.log(result)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+// User.findByIdAndUpdate('5fc5147e24b468322a72d9c9', { age: 1 })
+//   .then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1 })
+//   })
+//   .then((result) => {
+//     console.log(result)
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+
+const updateAgeAndCount = async (id, age) => {
+  await User.findByIdAndUpdate(id, { age })
+  const count = await User.countDocuments({ age })
+  return count
+}
+
+updateAgeAndCount('5fc5179b6f1ffb32b48bcc8e', 3)
+  .then((count) => console.log(count)) // eslint-disable-line
+  .catch((err) => console.log(err)) // eslint-disable-line
--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -1,14 +1,24 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5fc4fda1edf6e129f0337c8a')
-  .then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-  })
-  .then((result) => {
-    console.log(result)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+// Task.findByIdAndDelete('5fc4fda1edf6e129f0337c8a')
+//   .then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+//   })
+//   .then((result) => {
+//     console.log(result)
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+
+const deleteTaskAndCount = async (id) => {
+  await Task.findByIdAndDelete(id)
+  const count = await Task.countDocuments({ completed: false })
+  return count
+}
+
+deleteTaskAndCount('5fc4fdc5aebe652a20af86ae')
+  .then((count) => console.log(count)) // eslint-disable-line
+  .catch((err) => console.log(err)) // eslint-disable-line
