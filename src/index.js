2826630f55f04afef24fd8c6654672a90335417a
Aurélien Roux <aureroux13@gmail.com>
2020-12-04 18:14:34 -0500

    async await replaces promises

--- src/index.js
+++ src/index.js
@@ -10,55 +10,71 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 // USERS ROUTES
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body)
-  user
-    .save()
-    .then(() => res.status(201).send(user))
-    .catch((err) => res.status(400).send(err))
+
+  try {
+    await user.save()
+    res.status(201).send(user)
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
-app.get('/users', (req, res) => {
-  User.find({})
-    .then((users) => res.send(users))
-    .catch((err) => res.status(500).send(err))
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({})
+    res.send(users)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const { id } = req.params
-  User.findById({ _id: id })
-    .then((user) => {
-      if (!user) return res.status(404).send('No user found')
 
-      return res.send(user)
-    })
-    .catch((err) => res.status(500).send(err))
+  try {
+    const user = await User.findById({ _id: id })
+    if (!user) return res.status(404).send('No user found')
+
+    return res.send(user)
+  } catch (err) {
+    return res.status(500).send(err)
+  }
 })
 
 // TASKS ROUTES
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
-  task
-    .save()
-    .then(() => res.status(201).send(task))
-    .catch((err) => res.status(400).send(err))
+
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
-app.get('/tasks', (req, res) => {
-  Task.find({})
-    .then((tasks) => res.send(tasks))
-    .catch((err) => res.status(500).send(err))
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.send(tasks)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 })
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const { id } = req.params
-  Task.findById({ _id: id })
-    .then((task) => {
-      if (!task) return res.status(404).send('No task found')
 
-      return res.send(task)
-    })
-    .catch((err) => res.status(500).send(err))
+  try {
+    const task = await Task.findById({ _id: id })
+    if (!task) return res.status(404).send('No task found')
+
+    return res.send(task)
+  } catch (err) {
+    return res.status(500).send(err)
+  }
 })
 
 app.listen(port, () => console.log(`Server is live on port ${port}`))  // eslint-disable-line
