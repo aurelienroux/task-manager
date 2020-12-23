7c7c90e64517d9e826241c1d6d0ba973114fc834
Aurélien Roux <aureroux13@gmail.com>
2020-12-23 10:02:29 -0500

    120 paginating data

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,6 +18,7 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 // GET /tasks?completed=true
+// GET /tasks?limit=10&skip=10
 router.get('/tasks', auth, async (req, res) => {
   const match = {}
 
@@ -30,7 +31,11 @@ router.get('/tasks', auth, async (req, res) => {
     await req.user
       .populate({
         path: 'tasks',
-        match
+        match,
+        options: {
+          limit: parseInt(req.query.limit, 10),
+          skip: parseInt(req.query.skip, 10)
+        }
       })
       .execPopulate()
     res.send(req.user.tasks)
